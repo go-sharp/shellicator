6c148f32ad0c2704c18641887336ffc8daed7e35
sandro <snmed@users.noreply.github.com>
2019-10-14 22:45:08 +0200

    Update main.go
    Remove deleted api key

--- example/main.go
+++ example/main.go
@@ -9,8 +9,8 @@ import (
 
 func main() {
 	auth := shellicator.NewAuthenticator(shellicator.WithProvider("google",
-		providers.NewGoogleProvider("935256488604-ib4ju6dbif58kkq6ur3eq3bgq1pkamjl.apps.googleusercontent.com",
-			"FX1fCSiz9zfgPtjV7t2UFeN5")))
+		providers.NewGoogleProvider("<Client ID>",
+			"<Client ID>")))
 
 	fmt.Println(auth.Authenticate("google"))
 	fmt.Println(auth.GetToken("google"))
